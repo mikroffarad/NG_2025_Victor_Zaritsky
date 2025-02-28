4637d559d4a3cf2033a7f9d30510d8345478b5ec
Victor Zaritsky <viktorzarickiy@gmail.com>
2025-02-28 19:34:26 +0200

    added CreateComment procedure

--- Lesson_3/Task_1.sql
+++ Lesson_3/Task_1.sql
@@ -1,5 +1,5 @@
 ﻿-- Step 1: Create tables
-CREATE TABLE [User] (
+CREATE TABLE Users (
 	Id INT PRIMARY KEY IDENTITY,
 	Name NVARCHAR(100) NOT NULL,
 	SecondName NVARCHAR(100) NOT NULL
@@ -17,7 +17,7 @@ CREATE TABLE Project (
 	CreationDate DATETIME DEFAULT GETDATE() NOT NULL,
 	CreatorId INT NOT NULL,
 	CategoryId INT NOT NULL,
-	CONSTRAINT FK_Project_User FOREIGN KEY (CreatorId) REFERENCES [User](Id) ON DELETE CASCADE,
+	CONSTRAINT FK_Project_User FOREIGN KEY (CreatorId) REFERENCES Users(Id) ON DELETE CASCADE,
 	CONSTRAINT FK_Project_Category FOREIGN KEY (CategoryId) REFERENCES Category(Id) ON DELETE NO ACTION
 );
 
@@ -27,7 +27,7 @@ CREATE TABLE Comment (
 	Date DATETIME DEFAULT GETDATE() NOT NULL,
 	UserId INT NOT NULL,
 	ProjectId INT NOT NULL,
-	CONSTRAINT FK_Comment_User FOREIGN KEY (UserId) REFERENCES [User](Id) ON DELETE NO ACTION,
+	CONSTRAINT FK_Comment_User FOREIGN KEY (UserId) REFERENCES Users(Id) ON DELETE NO ACTION,
 	CONSTRAINT FK_Comment_Project FOREIGN KEY (ProjectId) REFERENCES [Project](Id) ON DELETE CASCADE
 );
 
@@ -36,13 +36,13 @@ CREATE TABLE Vote (
 	UserId INT NOT NULL,
 	ProjectId INT NOT NULL,
 	CONSTRAINT UQ_Vote_User_Project UNIQUE(UserId, ProjectId),
-	CONSTRAINT FK_Vote_User FOREIGN KEY (UserId) REFERENCES [User](Id) ON DELETE NO ACTION,
+	CONSTRAINT FK_Vote_User FOREIGN KEY (UserId) REFERENCES Users(Id) ON DELETE NO ACTION,
 	CONSTRAINT FK_Vote_Project FOREIGN KEY (ProjectId) REFERENCES Project(Id) ON DELETE CASCADE
 );
 GO
 
 -- Step 2: Add some users and categories
-INSERT INTO [User] (Name, SecondName)
+INSERT INTO Users (Name, SecondName)
 VALUES
 	('John', 'Doe'),
 	('Alice', 'Smith'),
@@ -54,7 +54,7 @@ VALUES
     ('Health');
 
 -- View User and Category tables
-SELECT * From [User];
+SELECT * From Users;
 SELECT * From Category;
 GO
 
@@ -63,11 +63,9 @@ CREATE PROCEDURE CreateProject
     @Name NVARCHAR(100),
     @Description NVARCHAR(500),
     @CreatorId INT,
-    @CategoryId INT,
-    @NewProjectId INT OUTPUT
+    @CategoryId INT
 AS
 BEGIN
-    SET NOCOUNT ON;
     BEGIN TRANSACTION;
     BEGIN TRY
         -- Check if the field with the same name and description exists
@@ -76,7 +74,6 @@ BEGIN
             WHERE Name = @Name AND Description = @Description
         )
         BEGIN
-            SET @NewProjectId = -1; -- Project already exists
             ROLLBACK TRANSACTION;
             RETURN;
         END
@@ -85,8 +82,77 @@ BEGIN
         INSERT INTO Project (Name, Description, CreationDate, CreatorId, CategoryId)
         VALUES (@Name, @Description, GETDATE(), @CreatorId, @CategoryId);
 
-        -- Returning Id of new project
-        SET @NewProjectId = SCOPE_IDENTITY();
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END
+GO
+
+-- Call procedure to create project
+-- CreateProject 1
+EXEC CreateProject
+	@Name = 'AI Startup',
+	@Description = 'An innovative AI project',
+	@CreatorId = 1,
+	@CategoryId = 1 -- Technology
+GO
+-- CreateProject 2
+EXEC CreateProject 
+    @Name = 'Online Learning Platform',
+    @Description = 'An interactive platform for online education',
+    @CreatorId = 2,
+    @CategoryId = 2 -- Education
+GO
+-- CreateProject 3 (shouldn't be created because project with the same name and description already exists)
+DECLARE @ProjectId INT;
+EXEC CreateProject
+	@Name = 'AI Startup',
+	@Description = 'An innovative AI project',
+	@CreatorId = 1,
+	@CategoryId = 1 -- Technology
+GO
+-- CreateProject 4
+DECLARE @ProjectId INT;
+EXEC CreateProject 
+    @Name = 'AI-Powered Diagnosis Tool',
+    @Description = 'An AI-based tool to assist doctors in diagnostics',
+    @CreatorId = 3,
+    @CategoryId = 3 -- Health
+GO
+
+-- View Project table, there should be 3 projects
+select * from project
+GO
+
+-- Step 4: CreateСomment procedure
+CREATE PROCEDURE CreateComment
+    @Text NVARCHAR(MAX),
+    @UserId INT,
+    @ProjectId INT
+AS
+BEGIN
+    BEGIN TRANSACTION;
+    BEGIN TRY
+        -- Check if user exists
+        IF NOT EXISTS (SELECT 1 FROM Users WHERE Id = @UserId)
+        BEGIN
+            ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+        -- Check if project exists
+        IF NOT EXISTS (SELECT 1 FROM Project WHERE Id = @ProjectId)
+        BEGIN
+	        ROLLBACK TRANSACTION;
+            RETURN;
+        END
+
+        -- Creating comment
+        INSERT INTO Comment (Text, Date, UserId, ProjectId)
+        VALUES (@Text, GETDATE(), @UserId, @ProjectId);
 
         COMMIT TRANSACTION;
     END TRY
@@ -95,16 +161,42 @@ BEGIN
         THROW;
     END CATCH
 END
+GO
 
--- Calling procedure to create project
-DECLARE @ProjectId INT;
-EXEC CreateProject
-	@Name = 'AI Startup',
-	@Description = 'An innovative AI project',
-	@CreatorId = 1,
-	@CategoryId = 1,
-	@NewProjectId = @ProjectId OUTPUT;
+-- CreateComment 1 (Valid comment)
+EXEC CreateComment 
+    @Text = 'This is a great project!', 
+    @UserId = 3, 
+    @ProjectId = 1;
+GO
+-- CreateComment 2 (Shouldn't be created because of non-exist user)
+EXEC CreateComment 
+    @Text = 'Invalid user test', 
+    @UserId = 999, 
+    @ProjectId = 1;
+GO
 
--- View Project table
-select * from project
+-- CreateComment 3 (Shouldn't be created because of non-exist project)
+EXEC CreateComment 
+    @Text = 'Invalid project test', 
+    @UserId = 1, 
+    @ProjectId = 999;
+GO
+
+-- CreateComment 4
+EXEC CreateComment 
+    @Text = 'Keep it up!', 
+    @UserId = 1, 
+    @ProjectId = 2;
+GO
+
+-- CreateComment 5
+EXEC CreateComment 
+    @Text = 'That is the best thing I have ever seen', 
+    @UserId = 2, 
+    @ProjectId = 3;
+GO
+
+-- View Project table, there should be 3 comments
+select * from Comment
 GO
